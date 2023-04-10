b8985d99e4338fb0a5c8a014b43d6d04a6e87168
BuildTools <46540330+willkroboth@users.noreply.github.com>
2023-04-10 14:13:40 -0400

    Add 1.15 and 1.16 Spigot build

---
+++ Spigot/build/1.16.3/run.sh
@@ -0,0 +1,8 @@
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 11.0.16.1-tem
+
+../clean.sh
+
+echo Rebuilding...
+java -jar ../BuildTools.jar --rev 1.16.3
+
---
+++ Spigot/build/1.16.4/run.sh
@@ -0,0 +1,7 @@
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 11.0.16.1-tem
+
+../clean.sh
+
+echo Rebuilding...
+java -jar ../BuildTools.jar --rev 1.16.4
