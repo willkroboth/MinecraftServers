5e016681012214efd7cbb66870db0dfa4b409368
BuildTools <46540330+willkroboth@users.noreply.github.com>
2023-10-27 11:31:16 -0400

    Add `Paper.IgnoreJavaVersion` flag to Spigot Server template

--- Spigot/servers/Server template/run.sh
+++ Spigot/servers/Server template/run.sh
@@ -1,3 +1,3 @@
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 17.0.3-tem
-java -Xmx1024M -Xms1024M -jar server.jar nogui
+java -DPaper.IgnoreJavaVersion=true -Xmx1024M -Xms1024M -jar server.jar nogui
